d1af4481fac20743f9b321b62f809aac8c12473d
Pavel Vodrážka <pavel.vodrazka@etnetera.cz>
2017-12-29 23:26:40 +0100

    Load local copy of Wishlist.txt.

--- hackvent2017/src/main/scala/hackvent2017/Day02.scala
+++ hackvent2017/src/main/scala/hackvent2017/Day02.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object Day02 {
 
   def main(args: Array[String]): Unit = {
-    var s = Source.fromURL("https://hackvent.hacking-lab.com/Wishlist.txt").mkString
+    var s = Source.fromFile("hackvent2017/challenges/day02/files/Wishlist.txt").mkString
     while (!s.startsWith("HV17")) {
       s = ascii(b64Decode(s.replaceAll("\n", "")))
     }
